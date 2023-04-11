3b3f6fcd9c3781c562ea3268fa52ba60e3cbbe09
Dimitri Andre <dimitri@ymagyn.com>
2023-04-11 14:54:06 +0200

    nested routing

---
+++ src/Pages/CreateProduct.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateProduct() {
+  return <div>Create Product</div>;
+}
+
+export default CreateProduct;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,25 @@ import Header from "./Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import CreateProduct from "./Pages/CreateProduct";
+import Product from "./Pages/Product";
+import ProductList from "./Pages/ProductList";
+import ProductDetails from "./Pages/ProductDetails";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="product">
+          <Route path="" element={<Product />}></Route>
+          <Route path="create" element={<CreateProduct />}></Route>
+          <Route path="list" element={<ProductList />}></Route>
+          <Route path="details" element={<ProductDetails />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
